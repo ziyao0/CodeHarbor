5a926a62f66fc6c1c2ade48ad8da57a416c6111c
ziyao <1337891505@qq.com>
2023-10-18 20:55:33 +0800

    publish: crypto

--- harbor-crypto/src/main/java/com/ziyao/harbor/crypto/asymmetric/AsymmetricDecryptor.java
+++ harbor-crypto/src/main/java/com/ziyao/harbor/crypto/asymmetric/AsymmetricDecryptor.java
@@ -7,13 +7,13 @@ package com.ziyao.harbor.crypto.asymmetric;
  * @author ziyao zhang
  * @since 2023/10/18
  */
-public interface AsymmetricDecryptor<T> {
+public interface AsymmetricDecryptor {
 
     /**
-     * 对给定的密文信息进行解密，解密秘钥由{@link T}提供
+     * 对给定的密文信息进行解密
      *
-     * @param bytes 密文信息
-     * @param keyType          提供的秘钥信息
+     * @param bytes   密文信息
+     * @param keyType 提供的秘钥信息
      * @return 返回解密后的信息
      */
     byte[] decrypt(byte[] bytes, KeyType keyType);
