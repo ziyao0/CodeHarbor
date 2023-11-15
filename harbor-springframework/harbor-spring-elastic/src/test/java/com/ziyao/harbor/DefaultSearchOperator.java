a288fbe7d1faa69babd3c108958ef7bd8813b833
ziyao <1337891505@qq.com>
2023-11-15 19:56:59 +0800

    test:

--- harbor-springframework/harbor-spring-elastic/src/test/java/com/ziyao/harbor/DefaultSearchOperator.java
+++ harbor-springframework/harbor-spring-elastic/src/test/java/com/ziyao/harbor/DefaultSearchOperator.java
@@ -1,12 +1,20 @@
 package com.ziyao.harbor;
 
 import com.ziyao.harbor.elastic.entity.User;
+import com.ziyao.harbor.elastic.support.BeanPropertyExtractor;
+import org.springframework.data.elasticsearch.core.mapping.ElasticsearchPersistentProperty;
 import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchMappingContext;
 import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchPersistentEntity;
-import org.springframework.data.elasticsearch.repository.support.ElasticsearchEntityInformation;
+import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchPersistentProperty;
 import org.springframework.data.elasticsearch.repository.support.ElasticsearchEntityInformationCreatorImpl;
+import org.springframework.data.mapping.PersistentPropertyAccessor;
+import org.springframework.data.mapping.model.Property;
+import org.springframework.data.mapping.model.SimpleTypeHolder;
 import org.springframework.data.util.TypeInformation;
 import org.springframework.stereotype.Service;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
 
 /**
  * @author ziyao zhang
@@ -51,7 +59,7 @@ public class DefaultSearchOperator extends SimpleElasticsearchPersistentEntity<U
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IntrospectionException, ClassNotFoundException {
 
         SimpleElasticsearchMappingContext simpleElasticsearchMappingContext = new SimpleElasticsearchMappingContext();
         ElasticsearchEntityInformationCreatorImpl creator = new ElasticsearchEntityInformationCreatorImpl(simpleElasticsearchMappingContext);
@@ -60,9 +68,33 @@ public class DefaultSearchOperator extends SimpleElasticsearchPersistentEntity<U
         user.setId("1");
         user.setAddr("海淀");
         user.setName("李四");
+        user.setAge(1L);
 
-        ElasticsearchEntityInformation<User, Object> information = creator.getEntityInformation(User.class);
-        Object id = information.getId(user);
-        System.out.println(information);
+
+        Object addr = BeanPropertyExtractor.extractProperty(user, User.class, "addr");
+
+        System.out.println(addr);
+
+        SimpleElasticsearchPersistentEntity<?> persistentEntity = simpleElasticsearchMappingContext
+                .getRequiredPersistentEntity(user.getClass());
+
+        PersistentPropertyAccessor<User> propertyAccessor = persistentEntity.getPropertyAccessor(user);
+
+        PropertyDescriptor descriptor1 = new PropertyDescriptor("age", user.getClass());
+        Class<?> propertyType = descriptor1.getPropertyType();
+
+
+        System.out.println(propertyType.getName());
+
+        Property property2 = Property.of(TypeInformation.of(user.getClass()), descriptor1);
+
+        SimpleElasticsearchPersistentProperty property = new SimpleElasticsearchPersistentProperty(property2,
+                persistentEntity, SimpleTypeHolder.DEFAULT);
+
+        Object property1 = propertyAccessor.getProperty(property);
+        Object cast = propertyType.cast(property1);
+        System.out.println(cast);
+
     }
+
 }
