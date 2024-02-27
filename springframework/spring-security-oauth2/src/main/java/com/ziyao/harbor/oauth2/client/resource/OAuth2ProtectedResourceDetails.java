ac0743c0bb3e0146ae66d47e144c214c64d46cfc
ziyao zhang <1337891505@qq.com>
2024-02-27 16:06:52 +0800

    publish: 完善oauth2

---
+++ springframework/spring-security-oauth2/src/main/java/com/ziyao/harbor/oauth2/client/resource/OAuth2ProtectedResourceDetails.java
@@ -0,0 +1,8 @@
+package com.ziyao.harbor.oauth2.client.resource;
+
+/**
+ * @author ziyao zhang
+ * @since 2024/2/27
+ */
+public interface OAuth2ProtectedResourceDetails {
+}
---
+++ springframework/spring-security-oauth2/src/main/java/com/ziyao/harbor/oauth2/provider/authentication/TokenExtractor.java
@@ -0,0 +1,17 @@
+package com.ziyao.harbor.oauth2.provider.authentication;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * @author ziyao zhang
+ * @since 2024/2/27
+ */
+public interface TokenExtractor {
+    /**
+     * Extract a token value from an incoming request without authentication.
+     *
+     * @param request the current ServletRequest
+     * @return an authentication token whose principal is an access token (or null if there is none)
+     */
+    Object extract(HttpServletRequest request);
+}
