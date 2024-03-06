4b22dc4200bebdd3c13d5f6fdaafbad0b4a8f2cb
ziyao <1337891505@qq.com>
2024-03-06 19:15:29 +0800

    refactor:

--- user-center/src/main/java/com/ziyao/harbor/usercenter/repository/redis/MenuTreeRepository.java
+++ user-center/src/main/java/com/ziyao/harbor/usercenter/repository/redis/MenuTreeRepository.java
@@ -1,17 +1,20 @@
 package com.ziyao.harbor.usercenter.repository.redis;
 
+import com.ziyao.harbor.data.redis.core.Expired;
 import com.ziyao.harbor.data.redis.core.RedisKey;
 import com.ziyao.harbor.data.redis.repository.KeyValueRepository;
 import com.ziyao.harbor.usercenter.entity.MenuTree;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author ziyao
  * @since 2023/4/23
  */
 @Repository
+@Expired(timeout = 7, unit = TimeUnit.DAYS)
 @RedisKey(format = "ziyao:harbor:user_center:menu_tree:{}:{}")
 public interface MenuTreeRepository extends KeyValueRepository<List<MenuTree>> {
 }
