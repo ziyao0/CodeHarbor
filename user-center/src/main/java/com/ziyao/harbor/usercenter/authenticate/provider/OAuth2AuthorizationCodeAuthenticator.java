019ff9c4fe8b7377634e1830662e060f0c0074bb
ziyao-zhang <48820729+ziyao0@users.noreply.github.com>
2024-06-04 22:53:00 +0800

    feat: docker

---
+++ user-center/src/main/java/com/ziyao/harbor/usercenter/authenticate/provider/OAuth2AuthorizationCodeAuthenticator.java
@@ -0,0 +1,17 @@
+package com.ziyao.harbor.usercenter.authenticate.provider;
+
+import com.ziyao.harbor.usercenter.authenticate.Authenticator;
+import com.ziyao.harbor.usercenter.authenticate.core.AuthenticatedRequest;
+import com.ziyao.harbor.usercenter.authenticate.core.AuthenticatedUser;
+
+/**
+ * @author ziyao zhang
+ * @time 2024/6/4
+ */
+public class OAuth2AuthorizationCodeAuthenticator implements Authenticator {
+
+    @Override
+    public AuthenticatedUser authenticate(AuthenticatedRequest authenticatedRequest) {
+        return null;
+    }
+}
---
+++ user-center/src/main/java/com/ziyao/harbor/usercenter/authenticate/provider/OAuth2RefreshTokenAuthenticator.java
@@ -0,0 +1,16 @@
+package com.ziyao.harbor.usercenter.authenticate.provider;
+
+import com.ziyao.harbor.usercenter.authenticate.Authenticator;
+import com.ziyao.harbor.usercenter.authenticate.core.AuthenticatedRequest;
+import com.ziyao.harbor.usercenter.authenticate.core.AuthenticatedUser;
+
+/**
+ * @author ziyao zhang
+ * @time 2024/6/4
+ */
+public class OAuth2RefreshTokenAuthenticator implements Authenticator {
+    @Override
+    public AuthenticatedUser authenticate(AuthenticatedRequest authenticatedRequest) {
+        return null;
+    }
+}
